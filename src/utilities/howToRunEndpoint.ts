a33b6db75ab5040f2bac9d7ca6f00a08a6d55313
Rahul Prajapati <95571941+iamroare@users.noreply.github.com>
2024-09-02 10:22:39 +0530

    for table creation sript

--- src/utilities/howToRunEndpoint.ts
+++ src/utilities/howToRunEndpoint.ts
@@ -46,3 +46,42 @@
 
 
 
+
+// table creation for catmeow
+// -- Table: public.catmeow
+
+// -- DROP TABLE IF EXISTS public.catmeow;
+
+// CREATE TABLE IF NOT EXISTS public.catmeow
+// (
+//     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+//     name character varying(255) COLLATE pg_catalog."default",
+//     bread character varying(255) COLLATE pg_catalog."default",
+//     details character varying(255) COLLATE pg_catalog."default",
+//     CONSTRAINT catmeow_pkey PRIMARY KEY (id)
+// )
+
+// TABLESPACE pg_default;
+
+// ALTER TABLE IF EXISTS public.catmeow
+//     OWNER to postgres;
+
+
+
+
+// table creation for userInfo
+// -- Table: public.user
+
+// -- DROP TABLE IF EXISTS public."user";
+
+// CREATE TABLE IF NOT EXISTS public."user"
+// (
+//     username character varying(255) COLLATE pg_catalog."default" NOT NULL,
+//     password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+//     CONSTRAINT "PK_78a916df40e02a9deb1c4b75edb" PRIMARY KEY (username)
+// )
+
+// TABLESPACE pg_default;
+
+// ALTER TABLE IF EXISTS public."user"
+//     OWNER to postgres;
